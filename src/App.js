1671a1d7e8770624486d431997bcbb16713689ec
baedol59 <baedol59@plesion.co.kr>
2020-03-16 15:48:35 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
+// swbae start
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Footer from './components/Footer';
+//
+import LogIn from './components/auth/LogIn';
+import Register from './components/auth/Register';
+import ForgotPassword from './components/auth/ForgotPassword';
+import ForgotPasswordVerification from './components/auth/ForgotPasswordVerification';
+import ChangePassword from './components/auth/ChangePassword';
+import ChangePasswordConfirm from './components/auth/ChangePasswordConfirm';
+import Welcome from './components/auth/Welcome';
+
+// swbae end
 Amplify.configure(aws_exports);
 
 class App extends Component {
+
+  // add by swbae Start
+  state = {
+    isAuthenticated: false,
+    isAuthenticating: true,
+    user: null
+  }
+  setAuthStatus = authenticated => {
+      this.setState({ isAuthenticated: authenticated });
+    }
+
+    setUser = user => {
+      this.setState({ user: user });
+    }
+
+    async componentDidMount() {
+      try {
+        const session = await Auth.currentSession();
+        this.setAuthStatus(true);
+        console.log(session);
+        const user = await Auth.currentAuthenticatedUser();
+        console.log(user);
+        this.setUser(user);
+      } catch(error) {
+        if (error !== 'No current user') {
+          console.log(error);
+        }
+        console.log ( error ) ;
+      }
+      this.setState({ isAuthenticating: false });
+    }
+  // add by swbae End
+
   render() {
+    const authProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      user: this.state.user,
+      setAuthStatus: this.setAuthStatus,
+      setUser: this.setUser
+    }
+    /*
     return (
       <div className="App">
         <header className="App-header">
@@ -26,7 +81,31 @@ class App extends Component {
         </header>
       </div>
     );
+    */
+    return (
+      !this.state.isAuthenticating &&
+      <div className="App">
+        <Router>
+          <div>
+            <Navbar auth={authProps} />
+            <Switch>
+              <Route exact path="/" render={(props) => <Home {...props} auth={authProps} />} />
+              <Route exact path="/login" render={(props) => <LogIn {...props} auth={authProps} />} />
+              <Route exact path="/register" render={(props) => <Register {...props} auth={authProps} />} />
+              <Route exact path="/forgotpassword" render={(props) => <ForgotPassword {...props} auth={authProps} />} />
+              <Route exact path="/forgotpasswordverification" render={(props) => <ForgotPasswordVerification {...props} auth={authProps} />} />
+              <Route exact path="/changepassword" render={(props) => <ChangePassword {...props} auth={authProps} />} />
+              <Route exact path="/changepasswordconfirmation" render={(props) => <ChangePasswordConfirm {...props} auth={authProps} />} />
+              <Route exact path="/welcome" render={(props) => <Welcome {...props} auth={authProps} />} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </div>
+    );
+
   }
 }
 
-export default withAuthenticator(App, true);
+//export default withAuthenticator(App, true);  // by swbae comment 
+export default App;
